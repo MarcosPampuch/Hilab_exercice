9d19c7bd8b94be8becb80dbd9c790dadc2385a0d
MarcosPampuch <marcospampuch@gmail.com>
2022-02-17 20:00:28 +0100

    Project Creation

---
+++ keys.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb 17 19:49:20 2022
+
+@author: User
+"""
+
+api_key = '9N6iJ5QxT70cFjKIGd7O4eadp'
+
+api_secret_key = 'bQacfSYk4kcUYhIpswpVW1JiUJQY8NtqjT3rAjOTdkdm2QnM9M'
+
+bearer_token = 'AAAAAAAAAAAAAAAAAAAAAD9pZQEAAAAAltf9rkm8GWMJ52SHf5tjRk8rKRo%3Ds0aQ4HxEkj3it3edkr5v6Q2k1LkTFj2zA0D6fQetzCHz9cNNDh'
