a452e5161fedbd6598d7b6a91bc61c43874b5933
MarcosPampuch <marcospampuch@gmil.com>
2022-02-17 23:51:33 -0300

    fixing

--- stream_functions.py
+++ stream_functions.py
@@ -94,13 +94,13 @@ def get_stream(set):
             )
         )
 #    with open('sample.json', 'w', encoding='utf-8') as f:        
-        for response_line in response.iter_lines():
-            if response_line:
-                json_response = json.loads(response_line)
-                print(json.dumps(json_response, indent=4, sort_keys=True))
+    for response_line in response.iter_lines():
+        if response_line:
+            json_response = json.loads(response_line)
+            print(json.dumps(json_response, indent=4, sort_keys=True))
 #                result = json.dumps(json_response, indent=4, sort_keys=True)
 #                json.dump(result, f, ensure_ascii=False)
-                print("\n#############FUNC GET STREAM#########\n")
+            print("\n#############FUNC GET STREAM#########\n")
 
 
 def main():
