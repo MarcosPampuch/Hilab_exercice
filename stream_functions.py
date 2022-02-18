c06f1ec927536f4623fa88c6103f018cb0da16e1
MarcosPampuch <marcospampuch@gmil.com>
2022-02-17 23:28:20 -0300

    succesfully printing tweets

--- stream_functions.py
+++ stream_functions.py
@@ -10,12 +10,12 @@ import os
 import json
 import sys
 sys.path.append(r"D:\Users\Marcos\Documents\teste\Exercice_Hilab")
-from keys import api_key, api_secret_key, bearer_token
+#from keys import api_key, api_secret_key, bearer_token
 
 # To set your enviornment variables in your terminal run the following line:
-export 'BEARER_TOKEN'='<your_bearer_token>'
-bearer_token = os.environ.get("BEARER_TOKEN")
-
+#export 'BEARER_TOKEN'='AAAAAAAAAAAAAAAAAAAAAD9pZQEAAAAAltf9rkm8GWMJ52SHf5tjRk8rKRo%3Ds0aQ4HxEkj3it3edkr5v6Q2k1LkTFj2zA0D6fQetzCHz9cNNDh'
+bearer_token= os.environ.get("BEARER_TOKEN")
+#print(bearer_token)
 
 def bearer_oauth(r):
     """
@@ -36,6 +36,7 @@ def get_rules():
             "Cannot get rules (HTTP {}): {}".format(response.status_code, response.text)
         )
     print(json.dumps(response.json()))
+    print("\n#############FUNC GET RULES#########\n")
     return response.json()
 
 
@@ -57,13 +58,15 @@ def delete_all_rules(rules):
             )
         )
     print(json.dumps(response.json()))
+    print("\n#############FUNC DELETE RULES#########\n")
 
 
 def set_rules(delete):
     # You can adjust the rules if needed
     sample_rules = [
-        {"value": "dog has:images", "tag": "dog pictures"},
-        {"value": "cat has:images -grumpy", "tag": "cat pictures"},
+        {"value": "Futebol lang:pt", "tag": "Soccer rule"},
+        {"value": "Saúde lang:pt", "tag": "Health rule"},
+        {"value": "Comida lang:pt", "tag": "Food rule"}
     ]
     payload = {"add": sample_rules}
     response = requests.post(
@@ -76,6 +79,7 @@ def set_rules(delete):
             "Cannot add rules (HTTP {}): {}".format(response.status_code, response.text)
         )
     print(json.dumps(response.json()))
+    print("\n#############FUNC SET RULES#########\n")
 
 
 def get_stream(set):
@@ -89,10 +93,14 @@ def get_stream(set):
                 response.status_code, response.text
             )
         )
-    for response_line in response.iter_lines():
-        if response_line:
-            json_response = json.loads(response_line)
-            print(json.dumps(json_response, indent=4, sort_keys=True))
+    with open('sample.json', 'w', encoding='utf-8') as f:        
+        for response_line in response.iter_lines():
+            if response_line:
+                json_response = json.loads(response_line)
+#               print(json.dumps(json_response, indent=4, sort_keys=True))
+                result = json.dumps(json_response, indent=4, sort_keys=True)
+                json.dump(result, f, ensure_ascii=False)
+                print("\n#############FUNC GET STREAM#########\n")
 
 
 def main():
